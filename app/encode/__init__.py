e1afc1837b41adfb5992179d43a8d34b5c233b20
teteugomes17 <t3t3u0327@gmail.com>
2024-05-02 12:11:21 -0300

    finishies encode

--- app/encode/__init__.py
+++ app/encode/__init__.py
@@ -1,3 +1,26 @@
-def encode_function(message, shift):
+def encode(message, shift):
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z']
+    partialStart = ''
+    partialEnd = ''
+    shiftedAlphabet = ''
+    encodedMessage = ''
     
-    return message 
+    if shift == 0:
+        shiftedAlphabet = alphabet
+    elif shift > 0 and shift < 26:
+        partialStart = alphabet[:shift]
+        partialEnd = alphabet[shift:]
+        shiftedAlphabet = partialEnd + partialStart 
+    elif shift < 0 and shift > -26:
+        shift = shift * -1
+        partialStart = alphabet[:shift]
+        partialEnd = alphabet[shift:]
+        shiftedAlphabet = partialEnd + partialStart    
+    else:
+        return("shift_error")
+    
+    for letter in message:
+        letter_index = alphabet.index(letter)
+        encodedMessage = encodedMessage + shiftedAlphabet[letter_index]
+        
+    return encodedMessage 
--- tests/test_encode.py
+++ tests/test_encode.py
@@ -1,9 +1,9 @@
 from app import create_app
-from app.encode import encode_function
+from app.encode import encode
 
 flask_app = create_app()
 
 def test_encode_function():
-    encoded_message = encode_function("ABCD",2)
+    encoded_message = encode("ABCD",2)
     assert encoded_message == "CDEF"
     
